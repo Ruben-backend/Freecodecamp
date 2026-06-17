bbd1a19e78ea3016f627e3c9f4b66dca0b214a49
Rubén <ruben.ethicalhacker@gmail.com>
2026-06-17 10:24:40 -0600

    Add files via upload

---
+++ bot_saludos.js
@@ -0,0 +1,42 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+let bot;
+bot = "teacherBot";
+
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+let botIntroduction;
+botIntroduction = ("My name is"+ " " + bot + "." );
+console.log(botIntroduction);
+
+let botLocationSentence
+botLocationSentence = ("I live in " + botLocation + ".");
+console.log(botLocationSentence);
+
+bot = "professorBot";
+console.log(bot);
+
+let nicknameIntroduction;
+nicknameIntroduction = ("My nickname is "+ bot + "." );
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+let nickname;
+nickname = ("My nickname is "+ bot + "." );
+console.log(nickname);
+
+let newNicknameGreeting;
+newNicknameGreeting = ("I love my nickname but I wish people would call me " + bot + ".");
+console.log(newNicknameGreeting);
+
+let favoriteSubject;
+favoriteSubject = ("Computer Science");
+console.log (favoriteSubject);
+
+let favoriteSubjectSentence;
+favoriteSubjectSentence = ("My favorite subject is " + favoriteSubject + ".");
+console.log(favoriteSubjectSentence);
+
+console.log("Well, it was nice to talk to you. Have a nice day!");
